d8bfa3477e34ab6b3cfef7494480ed0ead1a733f
Daniel Sidauruk <danielsidauruk11@gmail.com>
2023-02-01 14:23:48 +0700

    Menambahkan Konfigurasi Awal

---
+++ lib/core/constants.dart
@@ -0,0 +1,7 @@
+const String serverFailureMessage = 'Server Failure';
+const String tblWatchlist = 'watchlist';
+
+const apiKey = 'api_key=2174d146bb9c0eab47529b2e77d6b526';
+const baseURL = 'https://api.themoviedb.org/3';
+
+String baseImageURL(String path) => 'https://image.tmdb.org/t/p/w500$path';
